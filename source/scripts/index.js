4214e08da3232bf321ff4cf362d78d7bfc3ac08f
Anna <150624111+AnnetDev@users.noreply.github.com>
2024-05-20 19:09:59 +0200

    адаптивные декор эл-ты

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,10 @@
-/* в этот файл добавляет скрипты*/
+document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.querySelector('.main-header__toggle');
+  const menu = document.querySelector('.main-navigation');
+
+  menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('main-navigation--open');
+    menu.classList.toggle('main-navigation--closed');
+    menuToggle.classList.toggle('main-header__toggle--opened');
+  });
+});
